a1f879acff4e057c07723c46a1e237d02b6736da
Melkam Eziokwu <melkamev@gmail.com>
2025-09-24 18:09:55 -0500

    feat: add env config method

--- src/configs/mod.rs
+++ src/configs/mod.rs
@@ -12,3 +12,23 @@ pub struct AppConfig {
   pub env: String,
   pub mongo: MongoConfig,
 }
+
+impl AppConfig {
+  pub fn from_env() -> Self {
+    // Load .env only when compiled with "dotenv"
+    #[cfg(feature = "dotenv")]
+    dotenv().ok();
+
+    let env_mode = env::var("APP_ENV").unwrap_or_else(|_| "development".into());
+
+    let mongo = MongoConfig {
+      uri: env::var("MONGO_URI").expect("MONGO_URI must be set"),
+      db_name: env::var("MONGO_DB").expect("MONGO_DB must be set"),
+    };
+
+    Self {
+      env: env_mode,
+      mongo,
+    }
+  }
+}
